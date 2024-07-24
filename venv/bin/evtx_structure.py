e63df8bef05772801507d00899e1e154c1804c79
Rohith0007 <50866037+Rohith0007@users.noreply.github.com>
2024-07-24 15:50:30 -0400

    Commit

---
+++ venv/bin/evtx_structure.py
@@ -0,0 +1,4 @@
+#!/Users/rohithgupthakona/git/windowsEventViewer/venv/bin/python3
+# EASY-INSTALL-SCRIPT: 'python-evtx==0.8.0','evtx_structure.py'
+__requires__ = 'python-evtx==0.8.0'
+__import__('pkg_resources').run_script('python-evtx==0.8.0', 'evtx_structure.py')
